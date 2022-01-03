af537466a4be460bca5dd1b3e99c6626e8191407
Luis Fernando Salazar Rivera <lfsalazarrivera@gmail.com>
2022-01-03 13:00:00 -0600

    First commit

---
+++ MediaHandler/MediaHandlerWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MediaHandlerWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
